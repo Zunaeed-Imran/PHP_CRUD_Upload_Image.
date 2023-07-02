ec6b0dcb20811d2fda5cfc06e8242c4107b44b58
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-07-02 11:50:29 +0600

    Update insert.php
    Image upload in PHP

--- insert.php
+++ insert.php
@@ -1,14 +1,14 @@
 <?php
 include 'config.php';
 
-if(isset($_POST['upload'])){
+if(isset($_POST)){
   $name = $_POST['name'];
   $price = $_POST['price'];
   $image = $_FILES['image'];
-  print_r($_FILES['image']);
-  $img_loc = $_FILES['image']['tmp_name'];
-  $img_name = $_FILES['image']['name'];
-  $img_des = "uploadimage/".$img_name;
+  //print_r($_FILES['image']);
+  //$img_loc = $_FILES['image']['tmp_name'];
+  //$img_name = $_FILES['image']['name'];
+  //$img_des = "uploadimage/".$img_name;
   move_uploaded_file($img_loc, 'uploadimage/'.$img_name);
 
 
