8aefc75e0dfa3fe902e75e84fdeac6a36d79bc3e
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-07-02 12:10:59 +0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,19 @@
 <?php
 include 'config.php';
 
-if(isset($_POST)){
+if (isset($_POST)) {
   $name = $_POST['name'];
   $price = $_POST['price'];
   $image = $_FILES['image'];
-  //print_r($_FILES['image']);
-  //$img_loc = $_FILES['image']['tmp_name'];
-  //$img_name = $_FILES['image']['name'];
-  //$img_des = "uploadimage/".$img_name;
-  move_uploaded_file($img_loc, 'uploadimage/'.$img_name);
 
+  $img_tmp_name = $image['tmp_name'];
+  $img_name = $image['name'];
+  $img_destination = 'uploadimage/' . $img_name;
 
-  mysqli_query($con, "insert into `image2`(`name`, `price`, `image`) values ('$name', '$price', '$img_des')");
+  // Move the uploaded file to the desired destination
+  move_uploaded_file($img_tmp_name, $img_destination);
+
+  // Insert the file details into the database
+  $query = "INSERT INTO `image2` (`name`, `price`, `image`) VALUES ('$name', '$price', '$img_destination')";
+  mysqli_query($con, $query);
 }
